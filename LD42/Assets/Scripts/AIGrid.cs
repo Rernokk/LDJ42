19004a4a88b7abb97919a19dab7f6b8137e4dada
UFP\ssmith37 <ssmith37@uccs.edu>
2018-08-10 20:56:18 -0600

    Misc Recommit

---
+++ LD42/Assets/Scripts/AIGrid.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIGrid : MonoBehaviour {
+	GameObject[,] nodes;
+
+	[SerializeField]
+	GameObject nodePrefab;
+
+	[SerializeField]
+	float stepDistance;
+	
+	[SerializeField]
+	int mapWidth, mapHeight;
+
+	void Start(){
+		nodes = new GameObject[mapWidth, mapHeight];
+		for (int i = 0; i < mapWidth; i++){
+			for (int j = 0; j < mapHeight; j++){
+				nodes[i, j] = Instantiate(nodePrefab, new Vector3(i, j, 0), Quaternion.identity);
+			}
+		}
+	}
+}
