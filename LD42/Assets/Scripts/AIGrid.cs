1be6dc4ca7ac74404501e9fb7d532c456d3da2ea
UFP\ssmith37 <ssmith37@uccs.edu>
2018-08-10 21:04:24 -0600

    sync

--- LD42/Assets/Scripts/AIGrid.cs
+++ LD42/Assets/Scripts/AIGrid.cs
@@ -18,7 +18,7 @@ public class AIGrid : MonoBehaviour {
 		nodes = new GameObject[mapWidth, mapHeight];
 		for (int i = 0; i < mapWidth; i++){
 			for (int j = 0; j < mapHeight; j++){
-				nodes[i, j] = Instantiate(nodePrefab, new Vector3(i, j, 0), Quaternion.identity);
+				nodes[i, j] = Instantiate(nodePrefab, new Vector3(i + .5f - (mapWidth/2.0f), j + .5f - (mapHeight / 2.0f), 0), Quaternion.identity);
 			}
 		}
 	}
